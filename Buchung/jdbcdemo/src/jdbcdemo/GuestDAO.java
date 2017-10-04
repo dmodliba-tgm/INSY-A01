bc3f7d6353a269edca313230114f178446e0c3a0
Dzenan Suljic <dsuljic@student.tgm.ac.at>
2017-10-04 09:03:53 +0200

    comments were added. GUI is userfriendly

---
+++ Buchung/jdbcdemo/src/jdbcdemo/GuestDAO.java
@@ -0,0 +1,197 @@
+package jdbcdemo;
+
+import java.util.*;
+import java.sql.*;
+import java.io.*;
+
+/**
+ * Helper class for handling low-level JDBC calls to the database
+ *
+ */
+public class GuestDAO {
+
+	private Connection myConn;
+	
+	public GuestDAO() throws Exception {
+		
+		// connect to the database. Takes the information from flightdata.properties
+		
+		Properties props = new Properties();
+		props.load(new FileInputStream("flightdata.properties"));
+		
+		String user = props.getProperty("user");
+		String password = props.getProperty("password");
+		String dburl = props.getProperty("dburl");
+		
+		myConn = DriverManager.getConnection(dburl, user, password);
+		
+		System.out.println("DB connection successful to:" + dburl);
+		
+	}
+	
+	// Use the object theGuest for inserting information into the database
+	public void addGuest(Guest theGuest) throws Exception {
+		PreparedStatement myStmt = null;
+
+		try {
+			
+			//uses a placeholder and protects against SQL injection. Uses the column names firstname, lastname, etc
+			myStmt = myConn.prepareStatement("insert into passengers"
+					+ " (firstname, lastname, airline, flightnr, rownr, seatposition)"
+					+ " values (?, ?, ?, ?, ?, ?)");
+			
+			// value is set. 1 = first placeholder, 2 = second placeholder, etc.
+			myStmt.setString(1, theGuest.getFirstname());
+			myStmt.setString(2, theGuest.getLastname());
+			myStmt.setString(3, theGuest.getAirline());
+			myStmt.setInt(4, theGuest.getFlightnr());
+			myStmt.setInt(5, theGuest.getRownr());
+			myStmt.setString(6, theGuest.getSeatposition());
+			
+			// execute SQL
+			myStmt.executeUpdate();			
+		}
+		finally {
+			
+		}
+		
+	}
+	
+	/**
+	 * returns a list of all the guests, ordered by lastname
+	 * @throws Exception
+	 */
+	public List<Guest> getAllGuests() throws Exception {
+		
+		//empty list of guests
+		List<Guest> list = new ArrayList<>(); 						
+		
+		//null = placeholder
+		Statement myStmt = null; 
+		
+		// null = placeholder
+		ResultSet myRs = null; 										
+		
+		try {
+			
+			//creates an object for sending SQL statements to the database
+			myStmt = myConn.createStatement(); 					   
+			
+			 //executes the given SQL statement
+			myRs = myStmt.executeQuery("select * from passengers order by lastname");
+			
+			// for each element in the ResultSet
+			while (myRs.next()) { 				
+				// Takes the result set and creates a guest object and returns it
+				Guest tempGuest = convertRowToGuest(myRs); 	
+				// After the guest is created it adds the guest to the list
+				list.add(tempGuest); 					 		   
+			}
+
+			//returns the list after objects are added
+			return list; 
+		}
+		finally {
+			//closes the ResultSet and the Statement
+			close(myStmt, myRs); 									
+		}
+	}
+	
+	/**
+	 * search for guests by lastname
+	 * @param lastname
+	 * @return
+	 * @throws Exception
+	 */
+	public List<Guest> searchGuests(String lastname) throws Exception {
+		
+		//empty list of guests
+		List<Guest> list = new ArrayList<>(); 						 
+
+		//null = placeholder
+		PreparedStatement myStmt = null;	
+		
+		// null = placeholder
+		ResultSet myRs = null;										 
+
+		try {
+			
+			//whatever lastname the user writes. appended with the "%", so it can use the like command.
+			lastname += "%";										 
+
+			 //uses a placeholder and protects against SQL injection
+			myStmt = myConn.prepareStatement("select * from passengers where lastname like ? order by lastname");
+			
+			// value is set
+			myStmt.setString(1, lastname); 							
+			
+			//execute the given SQL statement
+			myRs = myStmt.executeQuery(); 							
+			
+			// for each element in the ResultSet
+			while (myRs.next()) { 	
+				
+				//Takes the result set and creates a guest object and returns it
+				Guest tempGuest = convertRowToGuest(myRs);	
+				
+				//After the guest is created it adds the guest to the list
+				list.add(tempGuest);								
+			}
+			
+			//returns the list after objects are added
+			return list;											 
+		}
+		finally {
+			
+			//closes the ResultSet and the Statement
+			close(myStmt, myRs);									
+		}
+	}
+	
+	/**
+	 * Takes the result set and creates a guest object and returns it
+	 * @param myRs
+	 * @return
+	 * @throws SQLException
+	 */
+	private Guest convertRowToGuest(ResultSet myRs) throws SQLException {
+		
+		int id = myRs.getInt("id");
+		String firstname = myRs.getString("firstname");
+		String lastname = myRs.getString("lastname");
+		String airline = myRs.getString("airline");
+		int flightnr = myRs.getInt("flightnr");
+		int rownr = myRs.getInt("rownr");
+		String seatposition = myRs.getString("seatposition");
+		
+		Guest tempGuest = new Guest(id, firstname, lastname, airline, flightnr, rownr, seatposition);
+		
+		return tempGuest;
+	}
+
+	
+	private static void close(Connection myConn, Statement myStmt, ResultSet myRs)
+			throws SQLException {
+
+		if (myRs != null) {
+			myRs.close();
+		}
+
+		if (myStmt != null) {
+			
+		}
+		
+		if (myConn != null) {
+			myConn.close();
+		}
+	}
+
+	private void close(Statement myStmt, ResultSet myRs) throws SQLException {
+		close(null, myStmt, myRs);		
+	}
+
+	public static void main(String[] args) throws Exception {
+		
+		GuestDAO dao = new GuestDAO();
+	}
+}
---
+++ Buchung/jdbcdemo/src/jdbcdemo/GuestsSearchApp.java
@@ -0,0 +1,175 @@
+package jdbcdemo;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.FlowLayout;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JOptionPane;
+import java.util.List;
+
+
+public class GuestsSearchApp extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField lastNameTextField;
+	private JButton btnSearch;
+	private JScrollPane scrollPane;
+	private JTable table;
+	
+	//reference to GuestDAO, because the GUI needs to have a handle to the DAO to call methods on it
+	private GuestDAO guestDAO;
+
+	private JPanel panel_1;
+	private JButton btnAddGuest;
+	
+	
+	/**
+	 * Launch the application
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GuestsSearchApp frame = new GuestsSearchApp();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	
+	
+	public GuestsSearchApp() {
+		
+		
+		// create the DAO
+		try {
+			guestDAO = new GuestDAO();
+		} catch (Exception exc) {
+			JOptionPane.showMessageDialog(this, "Error: " + exc, "Error", JOptionPane.ERROR_MESSAGE); // Errors during construction 
+		}
+		
+		
+		
+		
+		setTitle("Guest Search App");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
+		flowLayout.setAlignment(FlowLayout.LEFT);
+		contentPane.add(panel, BorderLayout.NORTH);
+		
+		JLabel lblEnterLastName = new JLabel("Enter last name");
+		panel.add(lblEnterLastName);
+		
+		lastNameTextField = new JTextField();
+		panel.add(lastNameTextField);
+		lastNameTextField.setColumns(10);
+		
+		
+		
+		
+		btnSearch = new JButton("Search");
+		btnSearch.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {			
+				
+				try {
+					
+					//get lastname from the textfield
+					String lastname = lastNameTextField.getText();			 
+
+					//empty guest list
+					List<Guest> guests = null; 								 
+
+					if (lastname != null && lastname.trim().length() > 0) {
+						
+						//if the textfield isnt empty, search guest by lastname (searchGuests)
+						guests = guestDAO.searchGuests(lastname);			
+					} else {
+						
+						// if textfield is empty, search all guests (getAllGuests)
+						guests = guestDAO.getAllGuests();					
+					}
+					
+					// create the table and update the "table"
+					// make use of the GuestTableModel. Passing the guests and the actual model
+					GuestTableModel model = new GuestTableModel(guests); 
+					
+					table.setModel(model);
+
+				} catch (Exception exc) {
+					
+					 //Error messages if necessary
+					JOptionPane.showMessageDialog(GuestsSearchApp.this, "Error: " + exc, "Error", JOptionPane.ERROR_MESSAGE);
+				}
+				
+			}
+		});
+		
+		
+		
+		
+		panel.add(btnSearch);
+		
+		scrollPane = new JScrollPane();
+		contentPane.add(scrollPane, BorderLayout.CENTER);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		
+		panel_1 = new JPanel();
+		contentPane.add(panel_1, BorderLayout.SOUTH);
+		
+		btnAddGuest = new JButton("Add Guest");
+		btnAddGuest.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				//creates a new dialog
+				AddGuests dialog = new AddGuests(GuestsSearchApp.this, guestDAO);
+				
+				//show dialog
+				dialog.setVisible(true);
+			}
+		});
+		panel_1.add(btnAddGuest);
+	}
+	
+	
+	
+	
+	// the guestview is being refresehed
+	public void refreshGuestsView() {
+		try {
+			
+			// A list of guests
+			List<Guest> guests = guestDAO.getAllGuests();
+			
+			// create a model and updates the table
+			GuestTableModel model = new GuestTableModel(guests);
+			
+			table.setModel(model);
+		}catch(Exception exc) {
+			JOptionPane.showMessageDialog(this, "Error: " + exc, "Error",
+					JOptionPane.ERROR_MESSAGE);
+		}
+	}
+
+}
